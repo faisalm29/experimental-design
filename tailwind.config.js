4084f8ae4b79d6fc61e9d13107f169e33817721e
faisalm29 <faisal.muhammad2911@gmail.com>
2022-07-31 10:34:40 +0800

    Changing style

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      heading: ["Eczar", "serif"],
+      body: ["Fira Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
